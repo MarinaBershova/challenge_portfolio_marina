5ccd45624ac80905cff0d6f5ebb7ce7d3814a619
Marina <marinabershova@gmail.com>
2022-08-15 22:33:14 +0200

    Added selectors

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,10 +1,14 @@
-# from pages.base_page import BasePage
-#
-#
-# class LoginPage(BasePage):
-#     login_field_xpath = "//*[@id='login']"
-#     password_field_xpath =
-#     sign_in_button_xpath =
-#
-#     def type_in_email(self, email):
-#         self.field_send_keys(self.login_field_xpath, email)
+ from pages.base_page import BasePage
+
+
+ class LoginPage(BasePage):
+     login_field_xpath = "//*[@id='login']"
+     password_field_xpath = "//*[@name="password"]"
+     remind_password_hyperlink_xpath = "//*[text()="Remind password"]"
+     choosing_language_xpath = "//*[@value="en"]"
+     sign_in_button_xpath = "//*[@type="submit"]"
+
+
+
+     def type_in_email(self, email):
+         self.field_send_keys(self.login_field_xpath, email)
