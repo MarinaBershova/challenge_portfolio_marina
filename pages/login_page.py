87177ee957c7156adfc98f0665aaaed41fd49d64
Marina <marinabershova@gmail.com>
2022-08-20 14:01:09 +0200

    Added selectors

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,10 +4,10 @@
  class LoginPage(BasePage):
      header_xpath = "//div/h5"
      login_field_xpath = "//*[@id='login']"
-     password_field_xpath = "//*[@name="password"]"
-     remind_password_hyperlink_xpath = "//*[text()="Remind password"]"
-     choosing_language_xpath = "//*[@value="en"]"
-     sign_in_button_xpath = "//*[@type="submit"]"
+     password_field_xpath = "//*[@name='password']"
+     remind_password_hyperlink_xpath = "//*[text()='Remind password']"
+     choosing_language_xpath = "//*[@value='en']"
+     sign_in_button_xpath = "//*[@type='submit']"
 
 
 
